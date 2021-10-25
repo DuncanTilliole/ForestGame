7be4366a3f906f37ccfa35f660012e8f9ab2e421
matthieucaballero <51957195+matthieucaballero@users.noreply.github.com>
2021-10-25 11:41:58 +0200

    Revert "Merge branch 'main' into MCA"
    This reverts commit 7485b2bb0963ef9688565aceb5c09af5cedaa854, reversing
    changes made to 4f4ecb88a4e5f83215d051bf1551faa2ce536dc2.

--- Assets/Projets/Scene/Scripts/FoxScript.cs
+++ Assets/Projets/Scene/Scripts/FoxScript.cs
@@ -4,28 +4,15 @@ using UnityEngine;
 
 public class FoxScript : MonoBehaviour
 {
-    public float acceleration = 1f;
-    private Animator animator;
-
     // Start is called before the first frame update
     void Start()
     {
-        animator = gameObject.GetComponent<Animator>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
-            animator.Play("Fox_Jump_InAir");
-        else if (Input.GetKey(KeyCode.DownArrow))
-            animator.Play("Fox_Falling");
-        else if (Input.GetKey(KeyCode.RightArrow))
-            animator.Play("Fox_Run_Right");
-        else if (Input.GetKey(KeyCode.LeftArrow))
-            animator.Play("Fox_Run_Left");
-
-        acceleration += 0.01f;
-        transform.Translate(Vector3.forward * acceleration * Time.deltaTime);
+        
     }
 }
